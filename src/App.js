8d0edc53d5675664045e8a992fa3d2a4ae923fa8
ansar-imam <ansar.digitalwaze@gmail.com>
2021-04-04 16:39:23 +0500

    first-push

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import "fontsource-roboto";
+
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import SaveIcon from "@material-ui/icons/Save";
+import DeleteIcon from "@material-ui/icons/Delete";
+import Checkbox from "@material-ui/core/Checkbox";
+
+function CheckBoxExample() {
+  const [checked, setChecked] = React.useState(true);
+  return (
+    <div>
+      <Checkbox
+        checked={checked}
+        color="secondary"
+        onChange={(e) => setChecked(e.target.checked)}
+      />
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <CheckBoxExample />
+        <ButtonGroup color="default">
+          <Button
+            //onClick={() => alert("hello")}
+            //href="#"
+            startIcon={<SaveIcon />}
+            style={{ fontSize: 14 }}
+            variant="contained"
+            //color="secondary"
+            //size="small"
+          >
+            Save
+          </Button>
+          <Button
+            startIcon={<DeleteIcon />}
+            style={{ fontSize: 14 }}
+            variant="contained"
+            //color="secondary"
+          >
+            Discard
+          </Button>
+        </ButtonGroup>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
